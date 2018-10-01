371f24b7301af046cfdc0e9df6dacd4b8626dcf5
20141074010169 <20141074010169@MC266470.ifrn.local>
2018-10-01 08:16:01 -0300

    Created Home Method

--- agendaspring/src/agendaspring/controllers/HomeController.java
+++ agendaspring/src/agendaspring/controllers/HomeController.java
@@ -1,6 +1,13 @@
 package agendaspring.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class HomeController {
+	@RequestMapping("/")
+	public void homeSeiLa() {
+		System.out.println("Chamou o método home!");
+	}
 
 }
